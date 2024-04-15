0770b08d909fea3b2033d3ffed0e8936d2db371a
Bruno <bsallesp@gmail.com>
2024-04-15 11:12:36 -0400

    Shuffled answer and options going good.

--- BrunoTheBot.CoreBusiness/Entities/Quiz/Question.cs
+++ BrunoTheBot.CoreBusiness/Entities/Quiz/Question.cs
@@ -21,5 +21,40 @@ namespace BrunoTheBot.CoreBusiness.Entities.Quiz
 
         [JsonPropertyName("Created")]
         public DateTime Created { get; set; } = DateTime.UtcNow;
+
+
+        public List<string> GetShuffledAnswerAndOptions()
+        {
+            List<string> shuffledList = new List<string>();
+
+            // Adiciona a resposta à lista
+            shuffledList.Add(Answer!);
+
+            // Adiciona todas as opções à lista
+            foreach (var option in Options)
+            {
+                shuffledList.Add(option.Name);
+            }
+
+            // Embaralha a lista
+            Shuffle(shuffledList);
+
+            return shuffledList;
+        }
+
+        // Método de embaralhamento
+        private static void Shuffle<T>(List<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
     }
 }
