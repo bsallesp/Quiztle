1e7ce965e373092cb049efbbd3a080d8b4222d0c
Bruno <bsallesp@gmail.com>
2024-04-18 08:25:47 -0400

    Regular game going good. Implementing Login User to keep Regular Game Going.

--- BrunoTheBot.CoreBusiness/Entities/Exam/RegularExam.cs
+++ BrunoTheBot.CoreBusiness/Entities/Exam/RegularExam.cs
@@ -11,21 +11,27 @@ namespace BrunoTheBot.CoreBusiness.Entities.Exam
 
         public int TotalQuestionsWithTrueAnswer()
         {
-            // Inicializa um contador para o total de perguntas com a chave "Answer" definida como verdadeira
             int total = 0;
 
-            // Itera sobre cada QuestionGameDTO na lista Questions
             foreach (var question in Questions)
             {
-                // Verifica se a opção com chave "Answer" está definida como verdadeira e incrementa o contador, se verdadeiro
                 if (question.Options.TryGetValue("Answer", out var option) && option.Item1)
                 {
                     total++;
                 }
             }
 
-            // Retorna o total de perguntas com a chave "Answer" definida como verdadeira
             return total;
+        }
+
+        public bool AllQuestionsAnsweredy()
+        {
+            return Questions.All(question => question.Options.Any(option => option.Value.Item1));
+        }
+
+        public int TotalQuestionsAnswered()
+        {
+            return Questions.Count(question => question.Options.Any(option => option.Value.Item1));
         }
     }
 }
