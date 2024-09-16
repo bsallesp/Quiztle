d1d0a965a65227b4384eca0e59f782b5fc5ba018
bsallesp <bsallesp@gmail.com>
2024-09-16 09:48:44 -0400

    more.

--- Quiztle.CoreBusiness/Entities/Scratch/Draft.cs
+++ Quiztle.CoreBusiness/Entities/Scratch/Draft.cs
@@ -7,5 +7,11 @@ namespace Quiztle.CoreBusiness.Entities.Scratch
         public Guid Id { get; set; }
         public required string Text { get; set; }
         public List<Question>? Questions { get; set; }
+        public int QuestionsAmountTarget = 10;
+
+        public bool NeedMoreQuestions()
+        {
+            return Questions == null || Questions.Count < QuestionsAmountTarget;
+        }
     }
 }
