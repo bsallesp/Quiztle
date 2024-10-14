2d96ebda6321393e0cd5c2fb08c80965574323fd
bsallesp <bsallesp@gmail.com>
2024-10-14 15:08:24 -0400

    Goind good.

--- Quiztle.API/Controllers/Tests/CreateTestByScratchController.cs
+++ Quiztle.API/Controllers/Tests/CreateTestByScratchController.cs
@@ -19,48 +19,8 @@ namespace Quiztle.API.Controllers
             _testRepository = testRepository;
         }
 
-        [HttpPost("{scratchId}")]
-        public async Task<IActionResult> ExecuteAsync(Guid scratchId, string testName, int questionsPerScratch)
-        {
-            if (testName is not null)
-            {
-                try
-                {
-                    var scratch = await _scratchRepository.GetScratchByIdAsync(scratchId);
-
-                    if (scratch == null) return NotFound($"Scratch with ID {scratchId} not found.");
-                    if (scratch.Drafts == null) return NotFound($"Drafts not found.");
-                    if (scratch.Drafts.Select(q => q.Questions) == null) return NotFound($"No questions found.");
-
-                    var questions = new List<Question>();
-
-                    foreach (var item in scratch.Drafts) questions.AddRange([.. item.GetRandomQuestions(questionsPerScratch)]);
-
-                    questions = questions.Where(v => v.Verified).ToList();
-
-                    Test test = new()
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = scratch.Name!,
-                        Questions = questions,
-                        Created = DateTime.UtcNow
-                    };
-
-                    await _scratchRepository.SaveChangesAsync(test);
-
-                    return Ok(test);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest($"Error creating test from draft: {ex.Message}");
-                }
-            }
-
-            throw new ArgumentNullException(nameof(testName));
-        }
-
         [HttpPost("{scratchId}/total")]
-        public async Task<IActionResult> ExecuteAsync(Guid scratchId, string testName, int totalQuestions, bool distributeEvenly)
+        public async Task<IActionResult> ExecuteAsync(Guid scratchId, string testName = "test1", int totalQuestions = 20, bool verifyQuestion = false)
         {
             if (string.IsNullOrWhiteSpace(testName))
                 return BadRequest("Test name is required.");
@@ -69,57 +29,94 @@ namespace Quiztle.API.Controllers
             {
                 var scratch = await _scratchRepository.GetScratchByIdAsync(scratchId);
                 if (scratch == null)
-                    return NotFound($"Scratch with ID {scratchId} not found.");
+                    throw new Exception($"Scratch with ID {scratchId} not found.");
 
-                if (scratch.Drafts == null || scratch.Drafts.Count == 0)
-                    return NotFound("No drafts available.");
+                if (scratch.Drafts == null || !scratch.Drafts.Any())
+                    throw new Exception("No drafts available.");
 
-                var totalDrafts = scratch.Drafts.Count;
-                var questionsPerDraft = totalQuestions / totalDrafts;
-                var remainingQuestions = totalQuestions % totalDrafts;
+                var test = new Test
+                {
+                    Id = Guid.NewGuid(),
+                    Name = testName,
+                    Questions = new List<Question>(),
+                    Created = DateTime.UtcNow
+                };
 
-                var questions = new List<Question>();
+                var drafts = scratch.Drafts.ToList();
+                int draftCount = drafts.Count;
+
+                for (int i = 0; i < totalQuestions; i++)
+                {
+                    int currentDraftIndex = i % draftCount;
+                    var draft = drafts[currentDraftIndex];
+                    var question = draft.GetRandomQuestions(1).FirstOrDefault();
+
+                    if (question != null)
+                    {
+                        if (!verifyQuestion || question.Verified)
+                        {
+                            test.Questions.Add(question);
+                        }
+                    }
+                }
+
+                if (test.Questions.Count < totalQuestions)
+                {
+                    return BadRequest($"Total questions collected: {test.Questions.Count}. {totalQuestions} are required.");
+                }
+
+                await _testRepository.CreateTestAsync(test);
+                return Ok(test);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error creating test from draft: {ex.Message}");
+            }
+        }
+
+        [HttpPost("{scratchId}/allquestions")]
+        public async Task<IActionResult> ExecuteAsync(Guid scratchId, string testName = "Teste1", bool verifyQuestion = false)
+        {
+            try
+            {
+                var scratch = await _scratchRepository.GetScratchByIdAsync(scratchId);
+                if (scratch == null)
+                    throw new Exception($"Scratch with ID {scratchId} not found.");
+
+                if (scratch.Drafts == null || !scratch.Drafts.Any())
+                    throw new Exception("No drafts available.");
+
+                var allQuestions = new List<Question>();
+
                 foreach (var draft in scratch.Drafts)
                 {
-                    var draftQuestions = draft.GetRandomQuestions(questionsPerDraft);
-
-                    // Distribute remaining questions evenly
-                    if (remainingQuestions > 0)
+                    var questions = draft!.Questions!.ToList();
+                    if (verifyQuestion)
                     {
-                        draftQuestions.AddRange(draft.GetRandomQuestions(1));
-                        remainingQuestions--;
+                        questions = questions.Where(q => q.Verified).ToList();
                     }
-
-                    questions.AddRange(draftQuestions);
+                    allQuestions.AddRange(questions);
                 }
 
-                // Ensure we do not exceed the total number of questions requested
-                while (questions.Count > totalQuestions)
+                if (!allQuestions.Any())
                 {
-                    questions.RemoveAt(new Random().Next(questions.Count));
-                }
-
-                // Ensure all questions are verified
-                questions = questions.Where(v => v.Verified).ToList();
-                if (questions.Count < totalQuestions)
-                {
-                    return BadRequest($"Total questions collected: {questions.Count}. {totalQuestions} are required.");
+                    return BadRequest("No questions available.");
                 }
 
                 var test = new Test
                 {
                     Id = Guid.NewGuid(),
                     Name = testName,
-                    Questions = questions,
+                    Questions = allQuestions,
                     Created = DateTime.UtcNow
                 };
 
-                await _testRepository.SaveChangesAsync();
+                await _testRepository.CreateTestAsync(test);
                 return Ok(test);
             }
             catch (Exception ex)
             {
-                return BadRequest($"Error creating test from draft: {ex.Message}");
+                return BadRequest($"Error creating test with all questions: {ex.Message}");
             }
         }
     }
