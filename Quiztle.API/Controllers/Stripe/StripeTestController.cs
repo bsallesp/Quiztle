29b3c9203360f1bd8032feeea5940c6cef1274c5
bsallesp <bsallesp@gmail.com>
2024-10-16 14:55:57 -0400

    going good

---
+++ Quiztle.API/Controllers/Stripe/StripeTestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace Quiztle.API.Controllers.Stripe
+{
+    [Route("api/webhooks/stripe")]
+    public class StripeTestController : Controller
+    {
+        public StripeTestController()
+        {
+            StripeConfiguration.ApiKey = "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"; // Replace with your key
+        }
+
+        [HttpGet("create")]
+        public IActionResult CreateResource(string customerId)
+        {
+            var options = new CustomerCreateOptions
+            {
+                Email = customerId
+            };
+
+            var service = new CustomerService();
+            Customer customer = service.Create(options);
+
+            Console.WriteLine(customer.Email);
+
+            return Ok(customer);
+        }
+
+        [HttpGet("check/{customerId}")]
+        public IActionResult CheckResource(string customerId)
+        {
+            var service = new CustomerService();
+            Customer customer = service.Get(customerId);
+
+            Console.WriteLine(customer.Email);
+
+            return Ok(customer);
+        }
+    }
+}
