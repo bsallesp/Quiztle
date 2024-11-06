785a6ea3b79ca32cbf7513a386cb9537061e8da4
bsallesp <bsallesp@gmail.com>
2024-11-06 14:30:07 -0500

    Unversioned Files.

---
+++ Quiztle.DataContext/Migrations/20241105153552_migration0017.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quiztle.DataContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration0017 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PriceInCents",
+                table: "Tests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceInCents",
+                table: "Tests");
+        }
+    }
+}
